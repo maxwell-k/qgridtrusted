ff643ecc2d39a67aa747793ac488fb867ce962d3
Keith Maxwell <keith.maxwell@gmail.com>
2024-01-14 16:55:45 +0000

    Refactor setup.py
    So that it is easier to follow
    
    Also hide `npm --version` output.

--- setup.py
+++ setup.py
@@ -1,125 +1,52 @@
-from setuptools import setup, Command
-from setuptools.command.sdist import sdist
+import logging
+from pathlib import Path
+from subprocess import run
+
+from setuptools import Command
+from setuptools import setup
 from setuptools.command.build_py import build_py
-from subprocess import check_call
-import logging as log
-import os
-import sys
-import platform
-from os.path import (
-    join, dirname, abspath, exists
-)
+from setuptools.command.sdist import sdist
 
-here = dirname(abspath(__file__))
-node_root = join(here, 'js')
-is_repo = exists(join(here, '.git'))
+STATIC = Path("qgrid") / "static"
+TARGETS = [STATIC / "extension.js", STATIC / "index.js"]
+STATIC.mkdir(exist_ok=True)
 
-log.basicConfig(level=log.DEBUG)
-log.info('setup.py entered')
-log.info('$PATH=%s' % os.environ['PATH'])
+logging.basicConfig(level=logging.DEBUG)
+logging.info("setup.py entered")
 
-def js_prerelease(command, strict=False):
-    """decorator for building minified js/css prior to another command"""
+
+def prepare(command):
     class DecoratedCommand(command):
         def run(self):
-            jsdeps = self.distribution.get_command_obj('jsdeps')
-            if not is_repo and all(exists(t) for t in jsdeps.targets):
-                # sdist, nothing to do
-                command.run(self)
-                return
-
-            try:
-                self.distribution.run_command('jsdeps')
-            except Exception as e:
-                missing = [t for t in jsdeps.targets if not exists(t)]
-                if strict or missing:
-                    log.warn('rebuilding js and css failed')
-                    if missing:
-                        log.error('missing files: %s' % missing)
-                    raise e
-                else:
-                    log.warn('rebuilding js and css failed (not a problem)')
-                    log.warn(str(e))
+            if not all(target.is_file() for target in TARGETS):
+                self.distribution.run_command("jsdeps")
             command.run(self)
-            update_package_data(self.distribution)
+
     return DecoratedCommand
 
-def update_package_data(distribution):
-    """update package_data to catch changes during setup"""
-    build_py = distribution.get_command_obj('build_py')
-    # re-init build_py options which load package_data
-    build_py.finalize_options()
 
-
-class NPM(Command):
-    description = 'install package.json dependencies using npm'
-
-    user_options = []
-
-    node_modules = join(node_root, 'node_modules')
-
-    targets = [
-        join(here, 'qgrid', 'static', 'extension.js'),
-        join(here, 'qgrid', 'static', 'index.js')
-    ]
+class jsdeps(Command):
+    def finalize_options(self):
+        pass
 
     def initialize_options(self):
         pass
 
-    def finalize_options(self):
-        pass
-
-    def has_npm(self):
-        try:
-            check_call(['npm', '--version'])
-            return True
-        except:
-            return False
-
-    def should_run_npm_install(self):
-        package_json = join(node_root, 'package.json')
-        node_modules_exists = exists(self.node_modules)
-        return self.has_npm()
-
     def run(self):
-        has_npm = self.has_npm()
-        if not has_npm:
-            log.error("`npm` unavailable.  If you're running this command using sudo, make sure `npm` is available to sudo")
+        try:
+            run(["npm", "--version"], capture_output=True, check=True)
+        except Exception:
+            logging.error("`npm` unavailable.")
+        else:
+            logging.info("Running `npm install`. This may take a while.")
+            run(["npm", "install"], cwd="js", check=True)
 
-        if self.should_run_npm_install():
-            log.info("Installing build dependencies with npm.  This may take a while...")
-            check_call(['npm', 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr)
-            os.utime(self.node_modules, None)
-
-        for t in self.targets:
-            if not exists(t):
-                msg = 'Missing file: %s' % t
-                if not has_npm:
-                    msg += '\nnpm is required to build a development version of a widget extension'
-                raise ValueError(msg)
-
-        # update package data in case this created new files
-        update_package_data(self.distribution)
-
-def package_files(directory):
-    paths = []
-    for (path, directories, filenames) in os.walk(directory):
-        for filename in filenames:
-            paths.append(os.path.join(path, filename))
-    return paths
-
-data_files = package_files('qgrid/static')
+        if missing := [i for i in TARGETS if not i.is_file()]:
+            raise ValueError("Missing files: %s" % missing)
 
 
-setup_args = {
-    'data_files': [
-        ('share/jupyter/nbextensions/qgrid', data_files),
-    ],
-    'cmdclass': {
-        'build_py': js_prerelease(build_py),
-        'sdist': js_prerelease(sdist, strict=True),
-        'jsdeps': NPM,
-    },
-}
-
-setup(**setup_args)
+data_files: list[tuple[str, list[str]]] = [
+    ("share/jupyter/nbextensions/qgrid", list(map(str, STATIC.iterdir()))),
+]
+cmdclass = dict(build_py=prepare(build_py), sdist=prepare(sdist), jsdeps=jsdeps)
+setup(data_files=data_files, cmdclass=cmdclass)
